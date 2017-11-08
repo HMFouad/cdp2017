4d398c5db421b411cff90cf63d4145026d060e9f
HMFouad <HMFouad@Yahoo.FR>
2017-11-08 16:46:06 +0100

    Ajout rest BD Us,Sprints, Tasks

--- BD/bd.sql
+++ BD/bd.sql
@@ -21,4 +21,36 @@ CREATE TABLE Acl (
        FOREIGN KEY (project_id) REFERENCES Projects(id)
 );
 
+CREATE TABLE Us (
+       id INT NOT NULL AUTO_INCREMENT,
+       description VARCHAR(100),
+       priority INT,
+       difficult INT,
+       state ENUM('Todo', 'Done'),
+       project_id INT,
+       FOREIGN KEY (project_id) REFERENCES Projects(id),
+       PRIMARY KEY (id)
+);
+
+CREATE TABLE Sprints (
+       id INT NOT NULL AUTO_INCREMENT,
+       date_begin DATE,
+       date_end DATE,
+       project_id INT,
+       FOREIGN KEY (project_id) REFERENCES Projects(id),
+       PRIMARY KEY (id)
+);
+
+CREATE TABLE Tasks (
+       id INT NOT NULL AUTO_INCREMENT,
+       description VARCHAR(100),
+       state ENUM('Todo', 'Doing', 'Done'),
+       user_id INT,
+       sprint_id INT,
+       FOREIGN KEY (user_id) REFERENCES Users(id),
+       FOREIGN KEY (sprint_id) REFERENCES Sprints(id),
+       PRIMARY KEY (id)
+);
+
+
 
