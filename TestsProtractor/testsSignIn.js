65de14234ca10b7fc6d6db42173e04dc563eb7f7
Guillaume Gamelas <guillaume.gamelas@gmail.com>
2017-11-14 22:43:22 +0100

    Ajout des débuts de test d'inscription

---
+++ TestsProtractor/testsSignIn.js
@@ -0,0 +1,63 @@
+
+
+describe('Tests for signing in', function() {
+  let baseURL = "file:///home/gg099/Projects/cdp2017/Front/";
+	let path = "Index.html";
+
+  let usernameField = element(by.id('username_inscr'));
+	let passwordField = element(by.id('password_inscr'));
+	let repeatPasswordField = element(by.id('rPassword_inscr'));
+	let signUpButton = element(by.id('signUp'));
+
+  let pwd  = '123456789'
+
+  function fillFields(name, pwd, repeatPwd){
+    usernameField.sendKeys(name);
+    passwordField.sendKeys(pwd);
+    repeatPasswordField.sendKeys(repeatPwd);
+  }
+
+  it('Good way', function(){
+    browser.get(baseURL+path);
+    fillFields('aaa', pwd, pwd);
+    signUpButton.click();
+    //TODO check que l'inscription est faite
+  });
+
+  it('Nothing filled', function(){
+    browser.get(baseURL+path);
+    signUpButton.click();
+    //TODO check que l'inscription n'est pas faite
+  });
+
+  it('No username', function(){
+    browser.get(baseURL+path);
+    fillFields('aab', pwd, pwd);
+    signUpButton.click();
+    //TODO check que l'inscription n'est pas faite
+  });
+
+  it('No password', function(){
+    browser.get(baseURL+path);
+    fillFields('aac', '', pwd);
+    signUpButton.click();
+    //TODO check que l'inscription n'est pas faite
+  });
+
+  it('No repeat password', function(){
+    browser.get(baseURL+path);
+    fillFields('aad', pwd, '');
+    signUpButton.click();
+    //TODO check que l'inscription n'est pas faite
+  });
+
+  it('Twice with same username', function(){
+    let name = 'aae';
+    browser.get(baseURL+path);
+    fillFields(name, pwd, pwd);
+    signUpButton.click();
+    fillFields(name, pwd, pwd);
+    signUpButton.click();
+    //TODO check que la 1ere est faite, mais pas la 2e
+  });
+});
