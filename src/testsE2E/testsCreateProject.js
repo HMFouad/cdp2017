c4fbdfc101516d7cda54b8fdfc905a9b6fd82b5c
Guillaume Gamelas <guillaume.gamelas@gmail.com>
2017-11-17 00:00:20 +0100

    tests fonctionnels (sauf pour la liste des projets)

--- src/testsE2E/testsCreateProject.js
+++ src/testsE2E/testsCreateProject.js
@@ -1,12 +1,17 @@
 describe('[Test] Create a project', function() {
-  let baseURL = "file:///home/gg099/Projects/cdp2017/Front/";
-	let path = "Index.html";
+  let baseURL = "http://localhost:8080/";
+	let path = "";
 
   let projectNameField = element(by.id('projectName'));
 	let projectDescrField = element(by.id('projectDescription'));
 	let createButton = element(by.id('create'));
 
-  let descr = 'Description d\'un projet foo'
+  let infoLabel = element(by.id('infoCreateProLabel'));
+  let stringDONE = "Project created.";
+  let stringPleaseFill = "Please, fill the name field.";
+  let stringWrongPwd = "Wrong password with this username";
+
+  let descr = 'Description d\'un projet foo';
 
   function fillFields(name, des){
     projectNameField.sendKeys(name);
@@ -17,38 +22,41 @@ describe('[Test] Create a project', function() {
     browser.get(baseURL+path);
     fillFields('fooa', descr);
     createButton.click();
-    //TODO check que le projet est créé
+    expect(infoLabel.getText()).toEqual(stringDONE);
   });
 
   it('with nothing', function(){
     browser.get(baseURL+path);
     fillFields('', '');
     createButton.click();
-    //TODO check que le projet est créé
+    expect(infoLabel.getText()).toEqual(stringPleaseFill);
   });
 
   it('without name', function(){
     browser.get(baseURL+path);
     fillFields('', descr);
     createButton.click();
-    //TODO check que le projet n'est pas créé
+    expect(infoLabel.getText()).toEqual(stringPleaseFill);
   });
 
   it('without description', function(){
     browser.get(baseURL+path);
     fillFields('foob', '');
     createButton.click();
-    //TODO check que le projet n'est pas créé
+    expect(infoLabel.getText()).toEqual(stringDONE);
   });
 
   it('twice with same name', function(){
     let name = 'fooc';
     browser.get(baseURL+path);
+    //1st
     fillFields(name, descr);
     signUpButton.click();
+    expect(infoLabel.getText()).toEqual(stringDONE);
+    //2nd
     fillFields(name, descr);
     signUpButton.click();
-    //TODO check que les 2 projets sont créés
+    expect(infoLabel.getText()).toEqual(stringDONE);
   });
 
 });
--- src/testsE2E/testsLogIn.js
+++ src/testsE2E/testsLogIn.js
@@ -1,10 +1,14 @@
 describe('[Test] Log in', function() {
-  let baseURL = "http://localhost:1337/";
+  let baseURL = "http://localhost:8080/";
 	let path = "";
 
   let usernameField = element(by.id('username_co'));
 	let passwordField = element(by.id('password_co'));
 	let logInButton = element(by.id('logIn'));
+
+  let infoLabel = element(by.id('infoLogLabel'));
+  let stringPleaseFill = "Please, fill all fields.";
+  let stringWrongPwd = "Wrong password with this username";
 
   let username = 'fooCo';
   let pwd = '123456789';
@@ -26,35 +30,35 @@ describe('[Test] Log in', function() {
     browser.get(baseURL+path);
     fillFields(username, pwd);
     logInButton.click();
-    //TODO check que la connexion est faite
+    expect(usernameCoLabel.getText()).toEqual(username);
   });
 
   it('with nothing', function(){
     browser.get(baseURL+path);
     fillFields('', '');
     logInButton.click();
-    //TODO check que la connexion n'est pas faite
+    expect(infoLabel.getText()).toEqual(stringPleaseFill);
   });
 
   it('without username', function(){
     browser.get(baseURL+path);
     fillFields('', pwd);
     logInButton.click();
-    //TODO check que la connexion n'est pas faite
+    expect(infoLabel.getText()).toEqual(stringPleaseFill);
   });
 
   it('without password', function(){
     browser.get(baseURL+path);
     fillFields(username, '');
     logInButton.click();
-    //TODO check que la connexion n'est pas faite
+    expect(infoLabel.getText()).toEqual(stringPleaseFill);
   });
 
   it('with wrong password', function(){
     browser.get(baseURL+path);
     fillFields(username, wrongPwd);
     logInButton.click();
-    //TODO check que la connexion n'est pas faite
+    expect(infoLabel.getText()).toEqual(stringWrongPwd);
   });
 
 });
