c83426a9e01c390077fba0bb00cc24ca82f4cdc9
Anne GNING <Anne GNING>
2017-12-12 00:29:11 +0100

    create Project non testé

--- GGHB/server/api.js
+++ GGHB/server/api.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const addUser = require('./services/addUser');
 const login = require('./services/tokens');
+const createProject = require('./services/createProject');
 
 router.use('/', addUser);
 router.use('/', login);
+router.use('/createProject', createProject);
 
 module.exports = router;
--- GGHB/src/app/create-project/create-project.component.ts
+++ GGHB/src/app/create-project/create-project.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'gghb-create-project',
   templateUrl: './create-project.component.html',
   styleUrls: ['./create-project.component.css']
 })
 export class CreateProjectComponent implements OnInit {
+  private createProjectForm: FormGroup;
 
-  constructor() { }
+  public constructor(private httpClient: HttpClient) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createProjectForm = new FormGroup({
+      projectName: new FormControl('', [Validators.required]),
+      projectDescription: new FormControl('', [Validators.required]),
+    });
   }
 
+  public get projectName () {
+    return this.createProjectForm.get('projectName');
+  }
+
+  public get projectDescription () {
+    return this.createProjectForm.get('projectDescription');
+  }
+
+  public submitScreateProjectForm () {
+ console.log ('Test0!!!!!!!!!!!!!!!!!');
+     if (this.createProjectForm.valid) {
+        this.httpClient.post(
+          '/api/createProject',
+          this.createProjectForm.value, {
+                responseType: 'json'
+            }).subscribe((response) => { // success
+              console.log ('Réponse!!!!!!!!!!!!!!!!!');
+              console.log (response);
+        }, (error) => { // error
+            console.log ('Erreur!!!!!!!!!!!!!!!!!');
+            console.log (error);
+        });
+     }else {console.log ('Not Valid');
+    }
+  }
 }
