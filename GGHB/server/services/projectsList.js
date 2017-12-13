5f862712f3fe6e6d25ef4d07985ee76e17c1e993
dylanbed <dylan.bedin@gmail.com>
2017-12-13 22:56:06 +0100

    affichage des projets d'un user fonctionnel

--- GGHB/server/services/projectsList.js
+++ GGHB/server/services/projectsList.js
@@ -3,17 +3,12 @@ const router = express.Router();
 const db_connexion = require('./../../bd/bd_connexion');
     let pro = {};
 // ProjectList service
-router.post('/projectslist', (req, res) => {
+router.post('/listProjects/:username', (req, res) => {
 
-
-
-    console.log("Service POST /projectslist");
-    var username_co = req.body.userName;
-    console.log("userName for ListProjects: " + username_co);
+    const username_co = req.params.username;
 
     if (username_co === null) {
-        res
-            .json({ success: false, message: "Missing userName" });
+        res.json({ success: false, message: "Missing userName" });
     }
     else {
         db_connexion.query('SELECT * FROM users WHERE username = ?', [username_co], function (error, user, fields) {
@@ -22,7 +17,6 @@ router.post('/projectslist', (req, res) => {
                 res
                     .json({ success: false, message: "error select username" });
             } else if (username_co == user[0].username) {
-
                 console.log("query select user: ok");
                 console.log("UserID is:  " + user[0].id);
 
@@ -31,23 +25,21 @@ router.post('/projectslist', (req, res) => {
                         console.log("query select project from acl: error");
                         res.json({ success: false, message: "User not found" });
                     } else {
-                        console.log('Number of projects for this user is   :   ' + acl.length);
+                      console.log('Number of projects for this user is   :   ' + acl.length);
+                      var projectsId = [];
 
-                        for (var i = 0; i < acl.length; i++) {
-
+                      for (var i = 0; i < acl.length; i++) {
+                        projectsId.push(acl[i].project_id);
+                      }
                             //Add token to users table
-                            db_connexion.query('SELECT * FROM projects WHERE id = ?', [acl[i].project_id], function (error, project, fields) {
+                            db_connexion.query('SELECT * FROM projects WHERE id in ( ? )', projectsId.join(), function (error, project, fields) {
                                 if (error) {
                                     console.log("Query select project: error ");
-                                } else {
-                                    pro = { id : project[0].id, 
-                                            name: project[0].project, 
-                                            description: project[0].description};
-                                    console.log('Project id   :   ' + pro.id);
+                                }
+                                else{
+                                  res.json(project);
                                 }
                             })
-                        }
-                        res.json(JSON.stringify(pro));
                     }
                 });
             } else {
